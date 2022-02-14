5b0eb46e163db993d4c488c802eb6ad0963e78d2
Jordim20 <99677503+Jordim20@users.noreply.github.com>
2022-02-14 12:54:41 -0400

    Add files via upload
    Para verificar si el código esta bien escrito ya que soy nuevo en esto y necesito saber si el proyecto funciona. Gracias!

---
+++ experimento de calculadora/Calculadora.js
@@ -0,0 +1,16 @@
+/* Este file(Calculadora) lo empezamos con mayuscula y por convencion los files que tiene clases se inician con mayuscula*/ 
+class Calculadora {
+    sumar( num1, num2){
+        return num1 + num2;
+    }
+    restar( num1, num2){
+       return num1 - num2;
+   }
+   dividir( num1, num2){
+       return num1 / num2;
+   }
+   multiplicar( num1, num2){
+       return num1 * num2;
+   }
+
+}
